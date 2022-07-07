3751a08746e06e350e670692336c4b5ae02883f0
srinidhi <91565397+srinidhi9831@users.noreply.github.com>
2022-07-07 14:42:05 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,7 +7,7 @@ return(
     <body>
 
 <div class="des-container">
-    <h1>Build fast, responsive sites with oneUI Srinidhi</h1>
+    <h1>Build fast, responsive sites with oneUI</h1>
     <p>Quickly design and customize responsive mobile-first sites with oneUI, front-end open source toolkit, featuring extensive prebuilt components.</p>
     <div>
         <button class="bg-primary btn-bigger">get started</button>
